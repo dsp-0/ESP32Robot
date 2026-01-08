97fc9129aca147166243b3fafafefa6639b48479
Serge Pyptev <spyptev@yandex.ru>
2026-01-09 01:49:37 +0300

    Исправлена ошибка setMinPreferred, часто встречающаяся в интернете. Видимо, была в примерах. Но сейчас там уже исправлена.

--- ESP32Robot.ino
+++ ESP32Robot.ino
@@ -331,7 +331,7 @@ void setup() {
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setMaxPreferred(0x12);
 
   BLEDevice::startAdvertising();
   Serial.println(6);
